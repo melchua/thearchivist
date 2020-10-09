c26180d8377624bd598531ecc3c26ab9d5119ed4
Mel Chua <mchuam@gmail.com>
2020-10-09 16:16:05 -0700

    Adding github app

--- services/githubApi.js
+++ services/githubApi.js
@@ -1,17 +1,58 @@
+//OLD for personal token
 const { Octokit } = require("@octokit/rest");
+const { createOAuthAppAuth } = require("@octokit/auth-oauth-app");
+
+// const auth = createOAuthAppAuth({
+//   clientId: process.env.CLIENT_ID,
+//   clientSecret: process.env.CLIENT_SECRET,
+// });
+
+// async function githubAuth() {
+//   const appAuthentication = await auth({
+//     type: "oauth-app",
+//   });
+//   console.log("appAuth", appAuthentication.headers.authorization);
+// }
+
+// const oauthtoken = githubAuth().headers;
+
+// Personal access token auth
+// const octokit = new Octokit({
+//   auth: process.env.GITHUB_PERSONAL_ACCESS_TOKEN,
+//   userAgent: process.env.USER_AGENT,
+// });
+
+// Basic Auth
+// const octokit = new Octokit({
+// auth: oauthtoken,
+// userAgent: process.env.USER_AGENT
+// });
+
+// createOAuthAppAuth?
 const octokit = new Octokit({
-  auth: process.env.GITHUB_PERSONAL_ACCESS_TOKEN,
-  userAgent: process.env.USER_AGENT,
+  authStrategy: createOAuthAppAuth,
+  auth: {
+    clientId: process.env.CLIENT_ID,
+    privateKey: process.env.PRIVATE_KEY,
+  },
 });
+
+// async function test() {
+//   const { data } = await octokit.request("/user");
+//   console.log("data", data);
+// }
+
+// test();
 
 const createCommentInIssue = async (owner, repo, issue_number, body) => {
   try {
-    await octokit.issues.createComment({
+    const result = await octokit.issues.createComment({
       owner,
       repo,
       issue_number,
       body,
     });
+    // console.log("result", result);
   } catch (e) {
     console.log("error: ", e);
   }
